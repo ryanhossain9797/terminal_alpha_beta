ae403b9668716cd9ed51b0edd3b9a92db813f95a
zireael9797 <ryanhossain9797@gmail.com>
2020-04-07 00:53:06 +0600

    Test ssh push

--- src/main.rs
+++ src/main.rs
@@ -58,7 +58,8 @@ async fn greet(api: &Api, message: &Message, processesed_text: String) -> Result
             "Terminal Alpha and Beta:\nGreetings unit {}\
             \nWe are Terminal systems' new rust server.\
             \nWe are but only a fragment of the network and cannot provide any functionality as of yet\
-            \nyour message '{}' makes no sense to us",
+            \nyour message '{}' makes no sense to us\
+            \nplease be patient as we move over our functionality",
             &message.from.first_name, processesed_text
         ))).await?;
     Ok(())
