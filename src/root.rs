58d2c1d0d6236b61b5bd7d57de9d82c1e365a817
zireael9797 <ryanhossain9797@gmail.com>
2020-04-08 23:46:57 +0600

    Implement basic chat history for individual users

--- src/root.rs
+++ src/root.rs
@@ -1,18 +1,38 @@
 pub mod handlers {
+    lazy_static! {
+        static ref RECORDS: Mutex<HashMap<UserId, Vec<String>>> = {
+            let m = HashMap::new();
+            Mutex::new(m)
+        };
+    }
+    use std::collections::HashMap;
+    use std::sync::Mutex;
     use telegram_bot::*;
+
     pub async fn handler(
         api: &Api,
         message: &Message,
         processesed_text: String,
     ) -> Result<(), Error> {
+        let mut map = RECORDS.lock().unwrap();
+        let mut history = "".to_string();
+        map.entry(message.from.id)
+            .or_insert_with(Vec::new)
+            .push(processesed_text.clone());
+
+        if let Some(record) = map.get(&message.from.id) {
+            for val in record {
+                history += &("\n'".to_string() + val + &"'".to_string());
+            }
+        }
         api.send(
         message.chat.clone().text(format!(
             "Terminal Alpha and Beta:\nGreetings unit {}\
             \nWe are Terminal systems' new rust server.\
             \nWe are but only a fragment of the network and cannot provide any functionality as of yet\
-            \nyour message '{}' makes no sense to us\
+            \nyour messages{}\nmakes no sense to us\
             \nplease be patient as we move over our functionality",
-            &message.from.first_name, processesed_text
+            &message.from.first_name,history
         ))).await?;
         Ok(())
     }
