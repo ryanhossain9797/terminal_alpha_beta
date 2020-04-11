56dd586f55d003e63f9f4681e13ebd11e6506084
zireael9797 <ryanhossain9797@gmail.com>
2020-04-11 21:40:27 +0600

    Restructure and refactor chat

--- src/root.rs
+++ src/root.rs
@@ -4,7 +4,7 @@ pub mod handlers {
 
     const WAITTIME: u64 = LONGWAIT;
     use std::collections::HashMap;
-    // use std::sync::Mutex;
+    use std::mem::drop;
     use std::time::{Duration, Instant};
     use telegram_bot::*;
     lazy_static! {
@@ -24,20 +24,70 @@ pub mod handlers {
         message: &Message,
         processesed_text: String,
     ) -> Result<(), Error> {
-        if processesed_text.starts_with("chat ") {
-            let msg = processesed_text.trim_start_matches("chat ").to_string();
-            let handler_assignment = chat(api.clone(), message.clone(), msg).await;
-            match handler_assignment {
-                Err(e) => println!("{:?}", e),
-                _ => (),
+        println!("processed text is '{}'", processesed_text);
+        let map = RECORDS.lock().await;
+        let entry_option = map.get(&message.from.id);
+        if let Some(record) = entry_option {
+            if record.state == "chat".to_string() {
+                drop(map);
+                println!("continuing chat");
+                let handler_assignment =
+                    continue_chat(api.clone(), message.clone(), processesed_text.clone()).await;
+                match handler_assignment {
+                    Err(e) => println!("{:?}", e),
+                    _ => (),
+                }
+            }
+        } else {
+            drop(map);
+            if processesed_text.starts_with("chat") {
+                println!("starting chat");
+                let start_chat = start_chat(api.clone(), message.clone()).await;
+                match start_chat {
+                    Err(e) => println!("{:?}", e),
+                    _ => (),
+                }
             }
         }
         Ok(())
     }
 
-    pub async fn chat(api: Api, message: Message, processesed_text: String) -> Result<(), Error> {
+    pub async fn start_chat(api: Api, message: Message) -> Result<(), Error> {
+        println!("START_CHAT: chat initiated");
+
         let mut map = RECORDS.lock().await;
+        map.entry(message.from.id)
+            .or_insert_with(|| UserStateRecord {
+                username: message.from.first_name.clone(),
+                chat: message.chat.id(),
+                last: Instant::now(),
+                state: "chat".to_string(),
+                history: Vec::new(),
+            });
+        drop(map);
+        println!("START_CHAT: record added");
+        api.send(message.chat.clone().text(format!(
+            "Terminal Alpha and Beta:\nGreetings unit {}\
+            \nwe will listen to your following queries",
+            &message.from.first_name
+        )))
+        .await?;
+        let wipe_launch = wipe_history(message.clone(), api.clone(), "chat".to_string()).await;
+        match wipe_launch {
+            Err(e) => println!("{:?}", e),
+            _ => (),
+        }
+        Ok(())
+    }
+
+    pub async fn continue_chat(
+        api: Api,
+        message: Message,
+        processesed_text: String,
+    ) -> Result<(), Error> {
         let mut history = "".to_string();
+
+        let mut map = RECORDS.lock().await;
         let entry = map
             .entry(message.from.id)
             .or_insert_with(|| UserStateRecord {
@@ -49,21 +99,19 @@ pub mod handlers {
             });
         entry.history.push(processesed_text.clone());
         entry.last = Instant::now();
-
         if let Some(record) = map.get(&message.from.id) {
             for val in &record.history {
                 history += &("\n'".to_string() + &val + &"'".to_string());
             }
         }
-        api.send(
-        message.chat.clone().text(format!(
+        drop(map);
+
+        api.send(message.chat.clone().text(format!(
             "Terminal Alpha and Beta:\nGreetings unit {}\
-            \nWe are Terminal systems' new rust server.\
-            \nWe are but only a fragment of the network and cannot provide any functionality as of yet\
-            \nyour messages{}\nmakes no sense to us\
-            \nplease be patient as we move over our functionality",
-            &message.from.first_name,history
-        ))).await?;
+            \nyour messages are{}",
+            &message.from.first_name, history
+        )))
+        .await?;
         let wipe_launch = wipe_history(message.clone(), api.clone(), "chat".to_string()).await;
         match wipe_launch {
             Err(e) => println!("{:?}", e),
@@ -79,6 +127,7 @@ pub mod handlers {
             if let Some(r) = map.get(&message.from.id) {
                 if r.last.elapsed() > Duration::from_secs(WAITTIME) && r.state == state {
                     map.remove(&message.from.id);
+                    drop(map);
                     println!("deleted chat record for {}", state);
                     let notice_result = api
                         .send(message.chat.text(format!(
