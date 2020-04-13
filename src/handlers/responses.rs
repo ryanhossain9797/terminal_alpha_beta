892111fc3889a4a693ede50bd3c4b13d83252c53
zireael9797 <ryanhossain9797@gmail.com>
2020-04-13 21:45:00 +0600

    Natural intent processing

--- src/handlers/responses.rs
+++ src/handlers/responses.rs
@@ -1,16 +1,4 @@
-//
-extern crate snips_nlu_lib;
-use snips_nlu_lib::SnipsNluEngine;
-//
-
 use telegram_bot::*;
-
-lazy_static! {
-    pub static ref ENGINE: SnipsNluEngine = {
-        println!("\nLoading the nlu engine...");
-        SnipsNluEngine::from_path("engine/").unwrap()
-    };
-}
 
 pub async fn unsupported_notice(api: Api, message: Message) -> Result<(), Error> {
     let notice_result = api
@@ -23,71 +11,6 @@ pub async fn unsupported_notice(api: Api, message: Message) -> Result<(), Error>
     match notice_result {
         Err(e) => println!("{:?}", e),
         _ => (),
-    }
-    Ok(())
-}
-
-pub async fn natural_understanding(
-    api: Api,
-    message: Message,
-    processed_text: String,
-) -> Result<(), Error> {
-    let intents_alternatives = 1;
-    let slots_alternatives = 1;
-
-    let result = ENGINE
-        .parse_with_alternatives(
-            &processed_text,
-            None,
-            None,
-            intents_alternatives,
-            slots_alternatives,
-        )
-        .unwrap();
-    if let Some(intent) = result.intent.intent_name {
-        println!(
-            "{} with confidence {}",
-            intent, result.intent.confidence_score
-        );
-        if result.intent.confidence_score > 0.5 {
-            if intent == "MakeTea" {
-                let notice_result = api
-                    .send(message.chat.text(format!(
-                        "you want us to make tea?\
-                \nwe do not know how to make human beverages"
-                    )))
-                    .await;
-                match notice_result {
-                    Err(e) => println!("{:?}", e),
-                    _ => (),
-                }
-            } else if intent == "MakeCoffee" {
-                let notice_result = api
-                    .send(message.chat.text(format!(
-                        "you want us to make coffee for you?\
-            \nwe do not know how to make human beverages"
-                    )))
-                    .await;
-                match notice_result {
-                    Err(e) => println!("{:?}", e),
-                    _ => (),
-                }
-            }
-        } else {
-            println!("unknown intent");
-            let handler_assignment = unsupported_notice(api.clone(), message.clone()).await;
-            match handler_assignment {
-                Err(e) => println!("{:?}", e),
-                _ => (),
-            }
-        }
-    } else {
-        println!("could not understand intent");
-        let handler_assignment = unsupported_notice(api.clone(), message.clone()).await;
-        match handler_assignment {
-            Err(e) => println!("{:?}", e),
-            _ => (),
-        }
     }
     Ok(())
 }
--- src/handlers/root.rs
+++ src/handlers/root.rs
@@ -7,9 +7,21 @@ use std::collections::HashMap;
 use std::mem::drop;
 use std::time::{Duration, Instant};
 use telegram_bot::*;
+
+//
+extern crate snips_nlu_lib;
+use snips_nlu_lib::SnipsNluEngine;
+//
 lazy_static! {
     pub static ref RECORDS: tokio::sync::Mutex<HashMap<UserId, UserStateRecord>> =
         { tokio::sync::Mutex::new(HashMap::new()) };
+}
+
+lazy_static! {
+    pub static ref ENGINE: SnipsNluEngine = {
+        println!("\nLoading the nlu engine...");
+        SnipsNluEngine::from_path("actionengine/").unwrap()
+    };
 }
 
 pub struct UserStateRecord {
@@ -19,6 +31,7 @@ pub struct UserStateRecord {
     pub chat: ChatId,
     pub history: Vec<String>,
 }
+
 pub async fn handler(
     api: &Api,
     message: &Message,
@@ -45,6 +58,15 @@ pub async fn handler(
                 Err(e) => println!("{:?}", e),
                 _ => (),
             }
+        } else {
+            drop(map);
+            let notice_result = api
+                .send(message.chat.text(format!("we cannot search yet")))
+                .await;
+            match notice_result {
+                Err(e) => println!("{:?}", e),
+                _ => (),
+            }
         }
     } else if will_respond {
         drop(map);
@@ -56,19 +78,9 @@ pub async fn handler(
                 Err(e) => println!("{:?}", e),
                 _ => (),
             }
-        }
-        // else {
-        //     let handler_assignment =
-        //         responses::unsupported_notice(api.clone(), message.clone()).await;
-        //     match handler_assignment {
-        //         Err(e) => println!("{:?}", e),
-        //         _ => (),
-        //     }
-        // }
-        else {
+        } else {
             let handler_assignment =
-                responses::natural_understanding(api.clone(), message.clone(), processesed_text)
-                    .await;
+                natural_understanding(api.clone(), message.clone(), processesed_text).await;
             match handler_assignment {
                 Err(e) => println!("{:?}", e),
                 _ => (),
@@ -77,6 +89,63 @@ pub async fn handler(
     }
     Ok(())
 }
+
+pub async fn natural_understanding(
+    api: Api,
+    message: Message,
+    processed_text: String,
+) -> Result<(), Error> {
+    let intents_alternatives = 1;
+    let slots_alternatives = 1;
+
+    let result = ENGINE
+        .parse_with_alternatives(
+            &processed_text,
+            None,
+            None,
+            intents_alternatives,
+            slots_alternatives,
+        )
+        .unwrap();
+    if let Some(intent) = result.intent.intent_name {
+        println!(
+            "{} with confidence {}",
+            intent, result.intent.confidence_score
+        );
+        if result.intent.confidence_score > 0.5 {
+            let response_result = if intent == "chat" {
+                println!("starting chat");
+                chat::start_chat(api.clone(), message.clone()).await
+            } else if intent == "search" {
+                println!("starting search");
+                start_search(api.clone(), message.clone()).await
+            } else {
+                responses::unsupported_notice(api.clone(), message.clone()).await
+            };
+            match response_result {
+                Err(e) => println!("{:?}", e),
+                _ => (),
+            }
+        } else {
+            println!("unknown intent");
+            let handler_assignment =
+                responses::unsupported_notice(api.clone(), message.clone()).await;
+            match handler_assignment {
+                Err(e) => println!("{:?}", e),
+                _ => (),
+            }
+        }
+    } else {
+        println!("could not understand intent");
+        let handler_assignment = responses::unsupported_notice(api.clone(), message.clone()).await;
+        match handler_assignment {
+            Err(e) => println!("{:?}", e),
+            _ => (),
+        }
+    }
+    Ok(())
+}
+
 pub async fn cancel_history(api: Api, message: Message) -> Result<(), Error> {
     let mut map = RECORDS.lock().await;
     map.remove(&message.from.id);
@@ -116,5 +185,33 @@ pub async fn wipe_history(message: Message, api: Api, state: String) -> Result<(
             }
         }
     });
+    Ok(())
+}
+
+pub async fn start_search(api: Api, message: Message) -> Result<(), Error> {
+    println!("START_SEARCH: search initiated");
+
+    let mut map = RECORDS.lock().await;
+    map.entry(message.from.id)
+        .or_insert_with(|| UserStateRecord {
+            username: message.from.first_name.clone(),
+            chat: message.chat.id(),
+            last: Instant::now(),
+            state: "search".to_string(),
+            history: Vec::new(),
+        });
+    drop(map);
+    println!("START_SEARCH: record added");
+    api.send(message.chat.clone().text(format!(
+        "Terminal Alpha and Beta:\nGreetings unit {}\
+            \nwhat do you want to search for?",
+        &message.from.first_name
+    )))
+    .await?;
+    let wipe_launch = wipe_history(message.clone(), api.clone(), "search".to_string()).await;
+    match wipe_launch {
+        Err(e) => println!("{:?}", e),
+        _ => (),
+    }
     Ok(())
 }
